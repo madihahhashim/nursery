b09b2f4762d39f00d4173848a3b52821202df43a
Madihah Hannani <57884148+madihahhashim@users.noreply.github.com>
2021-12-13 01:13:57 +0800

    Add files via upload

---
+++ connection/connection.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "nursery";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+/*if ($conn)
+    echo "connection ok";
+else
+   echo "connection tak ok";*/
+
+
+?>
